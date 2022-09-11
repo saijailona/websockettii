1215861e06035c1c5c98d35ef551b8f05d134d26
saijailona <sailotem@gmail.com>
2022-09-11 21:11:20 +0300

    first commit

---
+++ utils/messages.js
@@ -0,0 +1,12 @@
+const moment = require('moment');
+
+function formatMessage(username, text) {
+    return {
+        username,
+        text,
+    }
+}
+
+module.exports = {
+    formatMessage,
+};
